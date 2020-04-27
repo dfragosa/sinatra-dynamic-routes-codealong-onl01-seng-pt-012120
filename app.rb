a35ec0e9cfa5ec412479f04f66990a9659af25dc
Derric Fragosa <fderric@gmail.com>
2020-04-27 14:51:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,6 @@ class App < Sinatra::Base
   end
   
   get '/multiply' do
-    @num1 = params[:number1].to_i 
-    @num2 = params[:number2].to_i 
-    @num1 * @num2
+    " num1: #{params[:num1]}, times num2: #{params[:num2"]}
   end
 end
